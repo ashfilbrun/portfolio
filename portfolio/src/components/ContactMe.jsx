d1cb9771daec126bbb29b2ab50f5139e515ba7c4
ashfilbrun <ashesclaire@gmail.com>
2023-09-07 15:29:57 -0700

    add contact me updates

--- portfolio/src/components/ContactMe.jsx
+++ portfolio/src/components/ContactMe.jsx
@@ -1,9 +1,9 @@
 export default function ContactMe () {
   return (
-    <div>
-      <h2>Hey There!</h2>
-      <h1>If you'd like to get in touch with me you can: 
-      </h1>
+    <div className="contact">
+      <h1 className="title">Hey There!</h1>
+      <p>If you'd like to get in touch with me you can: 
+      </p>
       <p><a href="mailto:ashesclaire@gmail.com? subject=subject text">Send me an email!</a></p>
       <p>or</p>
       <p><a href="https://www.linkedin.com/in/ashlanfilbrun/">Connect with me on LinkedIn!</a></p>
