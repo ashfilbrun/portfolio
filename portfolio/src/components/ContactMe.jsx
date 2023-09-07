1df6b02ca2f5b56081173c0f61e775ba4e03db72
ashfilbrun <ashesclaire@gmail.com>
2023-09-07 15:35:50 -0700

    add font changes and styling fixes

--- portfolio/src/components/ContactMe.jsx
+++ portfolio/src/components/ContactMe.jsx
@@ -1,12 +1,14 @@
 export default function ContactMe () {
   return (
     <div className="contact">
-      <h1 className="title">Hey There!</h1>
-      <p>If you'd like to get in touch with me you can: 
-      </p>
-      <p><a href="mailto:ashesclaire@gmail.com? subject=subject text">Send me an email!</a></p>
-      <p>or</p>
-      <p><a href="https://www.linkedin.com/in/ashlanfilbrun/">Connect with me on LinkedIn!</a></p>
+      <h1 className="title" id="hcontact">Hey There!</h1>
+      <div className="contactContainer">
+        <p className="pcontainer">Want to get in touch with me? 
+        </p>
+        <p><a href="mailto:ashesclaire@gmail.com? subject=subject text">Send me an email!</a></p>
+        <p>or</p>
+        <p><a href="https://www.linkedin.com/in/ashlanfilbrun/">Connect with me on LinkedIn!</a></p>
+      </div>
     </div>
   )
 }
