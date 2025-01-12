f3c4d781dd02545fa8c22154986067470494649a
ashfilbrun <ashesclaire@gmail.com>
2025-01-11 20:44:05 -0700

    fix intro page font

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -8,8 +8,8 @@ export default function () {
           <div className="greeting">
             <h2 className="greet">Hello!</h2>
           </div>
-          <div className="intro">
-            <h2 className="explain">my name is</h2>
+          <div className="littleName">
+            <h2 className="name2">my name is</h2>
           </div>
           <div className="bigName">
             <h2 className="name">Ashlan Filbrun</h2>
