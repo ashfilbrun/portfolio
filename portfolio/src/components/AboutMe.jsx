c89acd67f4d80f819216654358faac2c3dc3eff9
ashfilbrun <ashesclaire@gmail.com>
2025-01-11 19:27:42 -0700

    edit and update about me page

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -5,13 +5,14 @@ export default function AboutMe () {
     <div>
       <h1 className='title'>About Me</h1>
       <div className="aboutMe">
-        <h2>Hi, I'm Ashlan Filbrun. Welcome!</h2>
-        <h3>I enjoy traveling the world, discovering new restaurants, going on adventures, keeping up with groundbreaking medical research, and geeking out on everything involving space (as you may be able to tell from a few of my projects),</h3>
-        <h3>and I am a software engineer.</h3>
-        <h3>I was born and raised in California and moved to super sunny Phoenix, Arizona in 2017. </h3>
+        <h2>Welcome!</h2>
+        <h3>Hi! My name is Ashlan. </h3>
+        <h3>I enjoy traveling the world, discovering new restaurants, going on adventures, keeping up with groundbreaking medical research, and geeking out on everything involving space (as you may be able to tell from a few of my projects). Currently in my free time I have been enjoying reading science fiction (Red Rising IYKYK) and playing Stardew Valley on my Switch.</h3>
+        <h3>I am a freshly hatched software engineer, who was born and raised in central California, and moved to Phoenix, Arizona in 2017. </h3>
         <h3> I am determined, hard-working, highly communicative, and constantly striving for growth in all aspects of my life, but especially as a software engineer and web developer.</h3>
+        <h3>My personal mantra for the past five years has been "Make it Happen!", which comes naturally to me as I'm the type of person to be relentlessly determined once I set my mind on something. So, as a self-development challenge for me for 2025, I am practicing NOT always making it happen.</h3>
         <h3>
-        I am looking for full-time employment as a software engineer and I hope you'll consider me for the role.</h3>
+        I am seeking to become a full-time member of a team within a company that values inclusivity, empowers growth, and supports its members' personal, as well as professional, development. My perfect fit would be within a team that challenges my skills, while supporting their further  development. If that sounds like your company, I hope you'll consider me for the role.</h3>
         {/* <span className="images">
           <img src='sedona1'></img>
         </span> */}
