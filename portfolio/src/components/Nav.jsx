6a49c53592e4158cea347b5489eecc5826f0effa
ashfilbrun <ashesclaire@gmail.com>
2025-01-11 21:10:45 -0700

    working on adding better nav bar functionality

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -2,16 +2,14 @@ import { Link } from "react-router-dom"
 
 export default function Nav () {
   return(
-    <div className="navBar">
-      <span className="nav">
-        <ul className="navList">
-          <li><Link to="/AboutMe">ABOUT ME</Link></li>
-          <li><Link to="/Projects">PROJECTS</Link></li>
-          <li><Link to="/SkillsList">SKILLS</Link></li>
-          <li><Link to='https://docs.google.com/document/d/1rzwiwpdr9qnRkkcQn3deKP5tswChxf2Mb44XuhZd_AU/edit?usp=sharing'>RESUMÉ</Link></li>
-          <li><Link to="/ContactMe">CONTACT ME</Link></li>
-        </ul>
-      </span>
+    <div className="nav">
+      <ul className="navList">
+        <li><Link to="/AboutMe">ABOUT ME</Link></li>
+        <li><Link to="/Projects">PROJECTS</Link></li>
+        <li><Link to="/SkillsList">SKILLS</Link></li>
+        <li><Link to='https://docs.google.com/document/d/1rzwiwpdr9qnRkkcQn3deKP5tswChxf2Mb44XuhZd_AU/edit?usp=sharing'>RESUMÉ</Link></li>
+        <li><Link to="/ContactMe">CONTACT ME</Link></li>
+      </ul>
     </div>
   )
 }
